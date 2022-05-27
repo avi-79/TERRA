cea03620310d9175235cc6c1ae19f7cd709474f0
root <root@ip-172-31-3-35.us-east-2.compute.internal>
2022-05-27 06:34:21 +0000

    new

---
+++ examples/vars.tf
@@ -0,0 +1,17 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-2 = "ami-0fa49cc9dc8d62c84"
+  }
+}
+
